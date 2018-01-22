174ac4b57c917b80ea59aebb15839165b81cab51
Matteo-Buonastella <matteobuonastella@gmail.com>
2018-01-22 10:21:51 -0500

    comm

--- js/article.js
+++ js/article.js
@@ -1,32 +1,177 @@
-////////////////////////////////ATALANTA////////////////////////////////////////
+//////////////////////////////GENERAL/////////////////////////////////////////
 
-function Atalanta3(){
+
+function General1(){
     document.getElementById("display3").style.display = "block";
     var article = document.getElementById("choose3")
     var title = "";
-    title += "<p>" + "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus" + "</p>" 
+    title += "<p>" + "Italy's failure to qualify for the World Cup for the first time in 60 years showed that soccer still has a unique knack for toppling the mighty, although such upsets are far more likely in the international game. The club game, on the other hand, is increasingly the preserve of a affluent few -- as shown by further titles for Real Madrid, Bayern Munich, Chelsea and Juventus. Four-times world champions Italy, last absent from the finals in 1958, were beaten 1-0 on aggregate by Sweden in a two-leg playoff, their fate being sealed by a goalless draw on a surreal November night at San Siro. The United States, South American champions Chile and three-times runners-up the Netherlands will join Italy on the sidelines as will Cameroon, winners of the African Nations Cup in Gabon at the start of the year." + "</p>"
+    + "<button class='btn btn-danger' + onclick = 'ShowLess();'>" + "Show Less" + "</button>";
+
+    article.innerHTML = title;
+}
+
+
+
+///////////////////////////ACMILAN//////////////////////////////////////////
+function ACMilan1(){
+    document.getElementById("ACMilan1").style.display = "block";
+    var article = document.getElementById("ACMilanchoose1")
+    var title = "";
+    title += "<p>" + "Milan got a crucial 3 points today in their win against Cagliari, however they did so unconvincingly and had to fight until the very end to squeeze out a narrow 2-1 victory after both teams had a man sent off late in the game. Cagliari took an early lead, however Milan bounced back with two successive goals and went into the break with momentum. Milan were unable to continue this momentum into the second half and looked sluggish. Cagliari looked no better and were sluggish for the remainder of the match. This win puts Milan into a potential Europa league spot, whereas Cagliari remain in mid table. " + "</p>" 
+    + "<button class='btn btn-danger' + onclick = 'ShowLess();'>" + "Show Less" + "</button>";
+
+    article.innerHTML = title;
+}
+
+
+
+/////////////////////////////ASROMA/////////////////////////////////////////
+
+function ASRoma1(){
+    document.getElementById("ASRoma1").style.display = "block";
+    var article = document.getElementById("ASRomachoose1")
+    var title = "";
+    title += "<p>" + "The highly anticipated showdown between Inter and Roma ended 1-1 at the Guissepe Meazza. Having comeback from a 2 week holiday, the players' lack of match fitness showed as the first half was riddled with errors by both teams. In what is turning up to be a highly contested battle for a champions league place, 3 teams (Roma, Lazio and Inter) are fighting for 2 spots. Roma took the lead in the first half through El Shaarawy after an error by Stanton saw the pacey winger through on goal and delecately chipped the ball over Handanovic into the empty goal. Inter would tie the game late on in the second half by a timely finish by Vecino. Inter will be the happier of the two sides as they were quite poor on the day " + "</p>" 
+    + "<button class='btn btn-danger' + onclick = 'ShowLess();'>" + "Show Less" + "</button>";
+
+    article.innerHTML = title;
+}
+
+
+
+
+///////////////////////////////ATALANTA////////////////////////////////////////
+
+function Atalanta3(){
+    document.getElementById("Atalanta3").style.display = "block";
+    var article = document.getElementById("Atalantachoose3")
+    var title = "";
+    title += "<p>" + "Not only is the provincial squad dreaming of a place in Europe but it has the Champions League within its sights after winning at Napoli 2-0 on Saturday to move to within three points of the third-placed side in Serie A. Defender Mattia Caldara again stole the show with a goal in each half. Atalanta midfielder Franck Kessie was sent off moments before Caldara’s second, following two bookings in quick succession. It ended Napoli’s run of 14 league matches without defeat. Inter Milan can move back level with Atalanta with a win over second-placed Roma on Sunday. Victory would make the race for a place in Europe’s elite club competition wide open." + "</p>" 
     + "<button class='btn btn-danger' + onclick = 'ShowLess();'>" + "Show Less" + "</button>";
 
     article.innerHTML = title;
 }
 
 function Atalanta2(){
-    document.getElementById("display2").style.display = "block";
-    var article = document.getElementById("choose2")
+    document.getElementById("Atalanta2").style.display = "block";
+    var article = document.getElementById("Atalantachoose2")
     var title = "";
-    title += "<p>" + "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus" + "</p>"
+    title += "<p>" + "Atalanta guranteed their spot in the round of 32 of the Europa League with an absolutely smashing, 5-1 win at Everton. From start to finish, Atalanta were by far the better team and dominanted all areas of the pitch. Papu Gomez completely dominated the left flank and was key in creating 2 of the 5 goals. A late brace by substitute Cornelius means that Atalanat will top their group so long as they don't lose by more then 1 goal against Lyon in 3 weeks time." + "</p>"
     + "<button class='btn btn-danger' + onclick = 'ShowLess();'>" + "Show Less" + "</button>";
 
     article.innerHTML = title;
 }
 
 function Atalanta1(){
-    document.getElementById("display1").style.display = "block";
-    var article = document.getElementById("choose1")
+    document.getElementById("Atalanta1").style.display = "block";
+    var article = document.getElementById("Atalantachoose1")
     var title = "";
-    title += "<p>" + "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus" + "</p>"
+    title += "<p>" + "The 3-0 scoreline may be flattering, but it was a poor showing by Atalanta who netted twice in stoppage time to give themselves a comftorable win on paper. Hellas Verona will be dissapointed that they were unable to capitalize in the 34th and 71st minute, when they were gifted two glorious oppurtunities. Poor finishing means that Atalanta get the 3 points, while Hellas continue to struggle towards the bottom of the table." + "</p>"
     + "<button class='btn btn-danger' + onclick = 'ShowLess();'>" + "Show Less" + "</button>";
 
+    article.innerHTML = title;
+}
+
+
+///////////////////////////////////BOLOGNA///////////////////////////////////
+
+function Bologna1(){
+    document.getElementById("Bologna1").style.display = "block";
+    var article = document.getElementById("Bolognachoose1")
+    var title = "";
+    title += "<p>" + "Bologna easily took care of Benevento and were 3-0 victors at the Stadio Renato Dall'Ara. Goals from Destro, De Maio and newly returned Blerim Dzemaili ensured that Bologna would comftorably finnish the match. Benevento rarely threatened in the offensive third and are in dire need of not just draws, but wins if they are to claw themselves out of the relegation spot. Bologna move to a respectable 12th spot in the table" + "</p>"
+    + "<button class='btn btn-danger' + onclick = 'ShowLess();'>" + "Show Less" + "</button>";
+
+    article.innerHTML = title;
+}
+
+
+//////////////////////////////////BENEVENTO////////////////////////////////
+
+function Benevento1(){
+    document.getElementById("Benevento1").style.display = "block";
+    var article = document.getElementById("Beneventochoose1")
+    var title = "";
+    title += "<p>" + "Bologna easily took care of Benevento and were 3-0 victors at the Stadio Renato Dall'Ara. Goals from Destro, De Maio and newly returned Blerim Dzemaili ensured that Bologna would comftorably finnish the match. Benevento rarely threatened in the offensive third and are in dire need of not just draws, but wins if they are to claw themselves out of the relegation spot. Bologna move to a respectable 12th spot in the table" + "</p>"
+    + "<button class='btn btn-danger' + onclick = 'ShowLess();'>" + "Show Less" + "</button>";
+
+    article.innerHTML = title;
+}
+
+
+/////////////////////////////////////CAGLIARI/////////////////////////////////
+
+function Cagliari1(){
+    document.getElementById("Cagliari").style.display = "block";
+    var article = document.getElementById("Cagliarichoose1")
+    var title = "";
+    title += "<p>" + "Milan got a crucial 3 points today in their win against Cagliari, however they did so unconvincingly and had to fight until the very end to squeeze out a narrow 2-1 victory after both teams had a man sent off late in the game. Cagliari took an early lead, however Milan bounced back with two successive goals and went into the break with momentum. Milan were unable to continue this momentum into the second half and looked sluggish. Cagliari looked no better and were sluggish for the remainder of the match. This win puts Milan into a potential Europa league spot, whereas Cagliari remain in mid table. " + "</p>" 
+    + "<button class='btn btn-danger' + onclick = 'ShowLess();'>" + "Show Less" + "</button>";
+
+    article.innerHTML = title;
+}
+
+
+////////////////////////////////////CHIEVO//////////////////////////////////
+
+function Chievo1(){
+    document.getElementById("Chievo1").style.display = "block";
+    var article = document.getElementById("Chievochoose1")
+    var title = "";
+    title += "<p>" + "It was men against boys as Lazio once again score 5, this time in a 5-1 trouncing of Chievo Verona. Goals came fast and hard as Lazio propell themselves into 3rd place and the elusive Champions League spot that they so desperately want. Chievo on the other hand continue to drop in the tables after a run of poor form sees them drop into 13th position. Their defense which seemed so formidable at the start of the season is really struggling and with still over a week left in the transfer window, could use some reinforcements. " + "</p>" 
+    + "<button class='btn btn-danger' + onclick = 'ShowLess();'>" + "Show Less" + "</button>";
+    article.innerHTML = title;
+}
+
+
+//////////////////////////////////////INTERNAZIONALE/////////////////////////////
+
+function Inter1(){
+    document.getElementById("Inter1").style.display = "block";
+    var article = document.getElementById("Interchoose1")
+    var title = "";
+    title += "<p>" + "The highly anticipated showdown between Inter and Roma ended 1-1 at the Guissepe Meazza. Having comeback from a 2 week holiday, the players' lack of match fitness showed as the first half was riddled with errors by both teams. In what is turning up to be a highly contested battle for a champions league place, 3 teams (Roma, Lazio and Inter) are fighting for 2 spots. Roma took the lead in the first half through El Shaarawy after an error by Stanton saw the pacey winger through on goal and delecately chipped the ball over Handanovic into the empty goal. Inter would tie the game late on in the second half by a timely finish by Vecino. Inter will be the happier of the two sides as they were quite poor on the day " + "</p>" 
+    + "<button class='btn btn-danger' + onclick = 'ShowLess();'>" + "Show Less" + "</button>";
+
+    article.innerHTML = title;
+}
+
+
+/////////////////////////////////////LAZIO////////////////////////////////////////////////
+
+function Lazio1(){
+    document.getElementById("Lazio1").style.display = "block";
+    var article = document.getElementById("Laziochoose1")
+    var title = "";
+    title += "<p>" + "It was men against boys as Lazio once again score 5, this time in a 5-1 trouncing of Chievo Verona. Goals came fast and hard as Lazio propell themselves into 3rd place and the elusive Champions League spot that they so desperately want. Chievo on the other hand continue to drop in the tables after a run of poor form sees them drop into 13th position. Their defense which seemed so formidable at the start of the season is really struggling and with still over a week left in the transfer window, could use some reinforcements. " + "</p>" 
+    + "<button class='btn btn-danger' + onclick = 'ShowLess();'>" + "Show Less" + "</button>";
+
+    article.innerHTML = title;
+}
+
+
+////////////////////////////////////////SPAL//////////////////////////////////////////
+
+function SPAL1(){
+    document.getElementById("SPAL1").style.display = "block";
+    var article = document.getElementById("SPALchoose1")
+    var title = "";
+    title += "<p>" + "SPAL will be the happier of the two sides as it ends 1-1 in Udine. Both teams were well matched and chances were few and far between. Udinese's winning ends, leaving them in 9th place and SPAL pick up another cruicial point in their quest for survival. " + "</p>" 
+    + "<button class='btn btn-danger' + onclick = 'ShowLess();'>" + "Show Less" + "</button>";
+    article.innerHTML = title;
+}
+
+
+
+//////////////////////////////////////UDINESE////////////////////////////////////////////
+
+function Udinese1(){
+    document.getElementById("Udinese1").style.display = "block";
+    var article = document.getElementById("Udinesechoose1")
+    var title = "";
+    title += "<p>" + "SPAL will be the happier of the two sides as it ends 1-1 in Udine. Both teams were well matched and chances were few and far between. Udinese's winning ends, leaving them in 9th place and SPAL pick up another cruicial point in their quest for survival. " + "</p>" 
+    + "<button class='btn btn-danger' + onclick = 'ShowLess();'>" + "Show Less" + "</button>";
     article.innerHTML = title;
 }
 
